f9d4c27a45242f42b0fb75ba3ca267fe1a8a7a4f
Romil Parhwal <romilparhwal007@gmail.com>
2019-02-18 23:00:50 -0500

    Add Screen Variables and Computations to Home Activity
    setContentView not set yet

--- app/src/main/java/com/gamemasters/magiccat/GameHome.java
+++ app/src/main/java/com/gamemasters/magiccat/GameHome.java
@@ -3,13 +3,28 @@ package com.gamemasters.magiccat;
 import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 
 public class GameHome extends AppCompatActivity {
 
+    // Define Variables for Screen
+    Display display;
+    Point size;
+    int screenHeight;
+    int screenWidth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        display = getWindowManager().getDefaultDisplay();
+        size = new Point();
+
+        display.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
+
         setContentView(R.layout.activity_game_home);
     }
 }
