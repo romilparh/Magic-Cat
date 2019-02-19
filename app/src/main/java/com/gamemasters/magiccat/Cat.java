871be6b7be528bf8161e75ded3472eff1f34bc45
Romil Parhwal <romilparhwal007@gmail.com>
2019-02-18 23:20:50 -0500

    Add Declaration for Cat and Enemy (Player and Automated Bots)
    Initial Declaration

--- app/src/main/java/com/gamemasters/magiccat/Cat.java
+++ app/src/main/java/com/gamemasters/magiccat/Cat.java
@@ -1,4 +1,38 @@
 package com.gamemasters.magiccat;
 
+import android.content.Context;
+import android.graphics.Rect;
+
 public class Cat {
+    private Context context;
+
+    protected int xPosition;
+    protected int yPosition;
+
+    private int width;
+    private int height;
+
+    private Rect hitBox;
+
+    public Cat(Context c, int xPosition, int yPosition, int width, int height) {
+
+        this.context = c;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.width = width;
+        this.height = height;
+
+        this.hitBox = new Rect(this.xPosition,this.yPosition,this.xPosition+this.width, this.xPosition+yPosition);
+    }
+
+    public void updateHitbox() {
+        hitBox.left = this.xPosition;
+        hitBox.top = this.yPosition;
+        hitBox.right = this.xPosition + this.width;
+        hitBox.bottom = this.yPosition + this.height;
+    }
+
+    public Rect getHitbox() {
+        return this.hitBox;
+    }
 }
