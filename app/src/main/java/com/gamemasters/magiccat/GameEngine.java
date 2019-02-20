3a22485f2cb1b00095f2f0c955d87b3c4b2ea498
Romil Parhwal <romilparhwal007@gmail.com>
2019-02-20 12:32:36 -0500

    Update GameEngine: Move Enemy and Buggy Intersection (Not Working)

--- app/src/main/java/com/gamemasters/magiccat/GameEngine.java
+++ app/src/main/java/com/gamemasters/magiccat/GameEngine.java
@@ -52,14 +52,16 @@ public class GameEngine extends SurfaceView implements Runnable {
     public void run() {
         while (gameIsRunning == true) {
             updateGame();
+            updateEnemyPosition();
             drawGame();
             controlFPS();
+            Log.d("Game Updating","Game Running"+enemies.get(0).xPosition);
         }
     }
 
     public void controlFPS() {
         try {
-            gameThread.sleep(17);
+            gameThread.sleep(60);
         }
         catch (InterruptedException e) {
 
@@ -80,6 +82,9 @@ public class GameEngine extends SurfaceView implements Runnable {
         background = BitmapFactory.decodeResource(context.getResources(), R.drawable.bg);
         // resize the image to match the phone
         background = Bitmap.createScaledBitmap(background,this.screenWidth, this.screenHeight, false);
+
+        this.spawnCat();
+        this.spawnEnemy();
 
     }
 
@@ -117,17 +122,67 @@ public class GameEngine extends SurfaceView implements Runnable {
             paintbrush.setStyle(Paint.Style.STROKE);
             paintbrush.setStrokeWidth(10);
             paintbrush.setColor(Color.WHITE);
-            //canvas.drawRect(player.xPosition, player.yPosition,player.xPosition+SIZE, player.yPosition+SIZE, paintbrush);
+            canvas.drawRect(player.xPosition, player.yPosition,player.xPosition+SIZE, player.yPosition+SIZE, paintbrush);
 
+            for(int i = 0; i<enemies.size();i++){
+                canvas.drawRect(enemies.get(i).xPosition, enemies.get(i).yPosition,enemies.get(i).xPosition+SIZE, enemies.get(i).yPosition+SIZE, paintbrush);
+            }
             // --------------------------------
             holder.unlockCanvasAndPost(canvas);
         }
 
     }
 
-    public void updateGame() {
+    public void updateEnemyPosition() {
+
+        // Move The Enemy
+
+        double xDist = this.player.xPosition - this.enemies.get(0).xPosition;
+        double yDist = this.player.yPosition - this.enemies.get(0).yPosition;
+
+        double distance = Math.sqrt((xDist * xDist) + (yDist * yDist));
+
+        double xSpeed = xDist/distance;
+        double ySpeed = yDist/distance;
+
+        this.enemies.get(0).xPosition = enemies.get(0).xPosition+(int)(xSpeed*10);
+        this.enemies.get(0).yPosition = enemies.get(0).yPosition+(int)(ySpeed*10);
+
+        Log.d("Updated Game","Updated"+enemies.get(0).xPosition);
+
 
     }
 
 
+    public void updateGame(){
+
+        if (player.getHitbox().intersect(enemies.get(0).getHitbox())) {
+            enemies.remove(0);
+        }
+
+    }
+
+    private void spawnCat() {
+        // put player in middle of screen --> you may have to adjust the Y position
+        // depending on your device / emulator
+        this.player = new Cat(this.getContext(), (this.screenWidth/2)-50, (int)(0.7*this.screenHeight),100,100,0);
+    }
+
+    private void spawnEnemy(){
+        ArrayList<Integer> sign = new ArrayList<>();
+        sign.add(10);
+        enemies.add(new Enemy(this.getContext(),100,100,100,100,sign));
+    }
+
+    public void resetGame() {
+        // spawn finger back in starting position
+        try {
+            Thread.sleep(1000);
+            this.spawnEnemy();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
