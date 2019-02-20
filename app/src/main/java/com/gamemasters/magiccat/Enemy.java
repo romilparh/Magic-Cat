1b383da13684003f26a34db881017cfd53fd4358
Romil Parhwal <romilparhwal007@gmail.com>
2019-02-20 13:44:10 -0500

    Add updateHitbox Method for enemy

--- app/src/main/java/com/gamemasters/magiccat/Enemy.java
+++ app/src/main/java/com/gamemasters/magiccat/Enemy.java
@@ -12,17 +12,18 @@ public class Enemy {
     protected int xPosition;
     protected int yPosition;
 
+
     private int width;
     private int height;
 
     // Sign is for the type of enemy, 0 and 1 for vertical and horizontal: the only two types of enemies there.
-    private ArrayList <Integer> sign;
+    private ArrayList<Integer> sign;
 
     Rect hitBox;
 
-    public Enemy(Context c, int xPosition, int yPosition, int width, int height, ArrayList <Integer> sign) {
+    public Enemy(Context c, int xPosition, int yPosition, int width, int height, ArrayList<Integer> sign) {
 
-        this.sign =  new ArrayList();
+        this.sign = new ArrayList();
 
         this.context = c;
         this.xPosition = xPosition;
@@ -31,20 +32,41 @@ public class Enemy {
         this.height = height;
         this.sign = sign;
 
-        this.hitBox = new Rect(this.xPosition,this.yPosition,this.xPosition+this.width, this.xPosition+yPosition);
+        this.hitBox = new Rect(this.xPosition, this.yPosition, this.xPosition + this.width, this.xPosition + yPosition);
     }
 
     public void updateEnemyPosition(int xPosition, int yPosition) {
         this.xPosition = xPosition;
         this.yPosition = yPosition;
+        updateHitbox();
+
     }
 
-    public void setXPosition(int xPosition){
+
+    public void updateHitbox() {
+
+        // update the position of the hitbox
+        this.hitBox.top = this.yPosition;
+        this.hitBox.left = this.xPosition;
+        this.hitBox.right = this.xPosition + 100;
+        this.hitBox.bottom = this.yPosition + 100;
+    }
+
+
+    public void setXPosition(int xPosition) {
         this.xPosition = xPosition;
     }
 
-    public void setYPosition(int yPosition){
+    public void setYPosition(int yPosition) {
         this.yPosition = yPosition;
+    }
+
+    public int getxPosition() {
+        return xPosition;
+    }
+
+    public int getyPosition() {
+        return yPosition;
     }
 
     public Rect getHitbox() {
