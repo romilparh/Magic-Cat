f9ea16cb548dc528651904fa217b69a594bf82b4
Romil Parhwal <romilparhwal007@gmail.com>
2019-03-01 04:40:32 -0500

    Add Gestures Detection
    Add gesture detection through MotionEvent to help with enemy removal

--- app/src/main/java/com/gamemasters/magiccat/GameHome.java
+++ app/src/main/java/com/gamemasters/magiccat/GameHome.java
@@ -1,9 +1,12 @@
 package com.gamemasters.magiccat;
 
 import android.graphics.Point;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
+import android.view.MotionEvent;
 
 public class GameHome extends AppCompatActivity {
 
@@ -41,6 +44,36 @@ public class GameHome extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         gameEngine.resumeGame();
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        int action = MotionEventCompat.getActionMasked(event);
+
+        String DEBUG_TAG = "HELLO DEBUG";
+        switch (action) {
+            case (MotionEvent.ACTION_DOWN):
+                Log.d(DEBUG_TAG, "Action was DOWN");
+                for(int i=0;i<gameEngine.enemies.size();i++){
+                    gameEngine.enemies.remove(i);
+                }
+                return true;
+            case (MotionEvent.ACTION_UP):
+                Log.d(DEBUG_TAG, "Action was UP");
+                return true;
+            case (MotionEvent.ACTION_CANCEL):
+                Log.d(DEBUG_TAG, "Action was CANCEL");
+                return true;
+            case (MotionEvent.ACTION_OUTSIDE):
+                Log.d(DEBUG_TAG, "Movement occurred outside bounds " +
+                        "of current screen element");
+                return true;
+            default:
+                return super.onTouchEvent(event);
+        }
+
     }
 }
 
